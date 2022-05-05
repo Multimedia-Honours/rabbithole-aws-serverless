64c98e8e1670e1c34bc778a30750b8f6962eeb41
DeMeiring <u17074292@tuks.co.za>
2022-05-05 20:55:45 +0200

    amplify setup for rabbithole with auth

---
+++ rabbithole/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,30 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "authGetUser": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "auth": {
+        "rabbithole80cf7702": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "authAPI": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
