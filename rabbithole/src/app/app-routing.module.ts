1772a15517cbbec3d402323a28c656a42e480d27
Matt Harty <matthewnharty@gmail.com>
2022-05-06 20:16:58 +0200

    Added back all the code
    ( MINUS all of the fonts, svgs and pngs, please add them back! NB )

--- rabbithole/src/app/app-routing.module.ts
+++ rabbithole/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingpageComponent } from './components/home/landingpage/landingpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingpageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
