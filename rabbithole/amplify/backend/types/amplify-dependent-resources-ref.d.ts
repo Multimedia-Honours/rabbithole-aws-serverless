459f88d4f597e756a2453ab9e8b0ba20374aaa1f
Matt Harty <matthewnharty@gmail.com>
2022-05-17 22:08:08 +0200

    Re-added auth to the new CI/CD pipeline and amplify backend

--- rabbithole/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ rabbithole/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,21 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "rabbitholeAuthPreSignup": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    },
+    "auth": {
+        "rabbitholeAuth": {
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
