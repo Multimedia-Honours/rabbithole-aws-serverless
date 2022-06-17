c02e3834e857e91b6f8214a00fc1fcc8d6ae3464
DeMeiring <u17074292@tuks.co.za>
2022-06-17 20:18:21 +0200

    added manual domain checking

--- rabbithole/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ rabbithole/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,18 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "function": {
-        "getUser": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "rabbitholeAuthPreSignup": {
-            "Name": "string",
-            "Arn": "string",
-            "LambdaExecutionRole": "string",
-            "Region": "string"
-        }
-    },
     "auth": {
         "rabbitholeAuth": {
             "HostedUIDomain": "string",
@@ -22,23 +8,6 @@ export type AmplifyDependentResourcesAttributes = {
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
             "AppClientID": "string"
-        }
-    },
-    "storage": {
-        "user": {
-            "Name": "string",
-            "Arn": "string",
-            "StreamArn": "string",
-            "PartitionKeyName": "string",
-            "PartitionKeyType": "string",
-            "Region": "string"
-        }
-    },
-    "api": {
-        "userAPI": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
         }
     }
 }
