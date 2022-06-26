6e42fe73816714516ea388e37b1f735087017b37
Matt Harty <matthewnharty@gmail.com>
2022-06-26 22:38:26 +0200

    Revert "Merge branch 'main' into development"
    This reverts commit 7fa2146863be5a48f35f1dc4d224fef2971b56e6, reversing
    changes made to 2598bd7f774364042a9612c028631803ffe1fde8.

---
+++ rabbithole/src/app/sub-systems/claims/components/claims-background/claims-background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'claims-background',
+  templateUrl: './claims-background.component.html',
+  styleUrls: ['./claims-background.component.scss']
+})
+export class ClaimsBackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
