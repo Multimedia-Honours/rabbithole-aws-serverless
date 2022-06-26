1fcc0c22f9d1ecee0ca2b9805efe666309a689d8
Matt Harty <matthewnharty@gmail.com>
2022-06-26 22:18:57 +0200

    build fix

--- rabbithole/src/environments/environment.prod.ts
+++ rabbithole/src/environments/environment.prod.ts
@@ -1,9 +1,4 @@
 export const environment = {
-      ACCESS_KEY: 'AKIAXXFS7F5IAHAGX6EC',
-      SECRET_ACCESS_KEY: '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',
-      RYVER_API_KEY: 'uat-9d881bb6b3c073d6c3dcc04383c0b50720175d48',
-      CLAIM_SERVER_URL: 'https://dvs354pi97.execute-api.us-east-1.amazonaws.com/staging/claims',
-      EVENTS_SERVER_URL: 'https://5yzyldmqx8.execute-api.us-east-1.amazonaws.com/staging/events',
       production: true,
     };
   
